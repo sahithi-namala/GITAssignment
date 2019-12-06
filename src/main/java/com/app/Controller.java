b92c3ebc9ca243f2879a74bd250aa0a4505a14f1
Sahithi Namala <sahithi_namala@uhc.com>
2019-12-06 16:39:00 +0530

    Commit all files

---
+++ src/main/java/com/app/Controller.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class Controller {
+	
+	@RequestMapping("/welcome")
+	  public String getAllUsers() {
+		String welcome = "Welcome to Rest Api";
+		System.out.println("welcome");
+	    return welcome;
+	  }
+
+}
